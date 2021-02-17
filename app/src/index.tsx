bed38e3bd50e6d9fd3c2b40c7fd2002ca7336bc8
zacharyarney <zachary.arney@gmail.com>
2021-02-16 23:41:16 -0600

    adds Auth0Provider HOC to index.tsx

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="walter-dev.us.auth0.com"
+      clientId="FTzC2iap1XacRVggZpFKVgc0qd7BOZ0I"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
