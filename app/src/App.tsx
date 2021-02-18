1ec6c5cf66dbadbec278b68b9573c4f9bccbaeac
zacharyarney <zachary.arney@gmail.com>
2021-02-18 02:14:41 -0600

    conditionally rendering Profile

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import './App.css';
 import { LoginButton } from './components/LoginButton/LoginButton';
 import { Profile } from './components/Profile/Profile';
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+  const profile = isAuthenticated ? (<Profile />) : null;
   return (
     <div className="App">
       <LoginButton />
-      <Profile />
+      {profile}
     </div>
   );
 }
