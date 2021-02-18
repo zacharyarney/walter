94200e38d3a07a339db6c2bb26bbf90acea13331
zacharyarney <zachary.arney@gmail.com>
2021-02-18 02:12:27 -0600

    adds test protected endpoint

--- server/src/api/server.ts
+++ server/src/api/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
-
+import { checkJwt } from '../middleware/checkJwt';
 export const app = express();
 
 // Middleware
@@ -13,5 +13,13 @@ app.use(morgan('dev'));
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.json({
+    message: 'Hello, world!',
+  });
+});
+
+app.get('/private', checkJwt, (req, res) => {
+  res.json({
+    message: 'You should only be able to see this if you are authorized.',
+  });
 });
