f9539ff2e8b71ab2ba516aea29e871a8fa8fdf36
zacharyarney <zachary.arney@gmail.com>
2021-02-18 02:13:21 -0600

    adds audience and api uri to client auth config

--- app/src/config/auth.config.ts
+++ app/src/config/auth.config.ts
@@ -1,4 +1,6 @@
 export default {
   domain: process.env.REACT_APP_AUTH0_DOMAIN ?? '',
   clientId: process.env.REACT_APP_AUTH0_CLIENT_ID ?? '',
-}
+  audience: process.env.REACT_APP_WALTER_API_AUDIENCE ?? '',
+  walterApiUri: process.env.REACT_APP_WALTER_API_URI ?? '',
+};
