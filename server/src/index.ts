57574779e2e19efcf7b11b053b4f4bca3a73d938
zacharyarney <zachary.arney@gmail.com>
2021-02-19 21:20:45 -0600

    connects server to mongodb atlas cluster

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,20 @@
+import { MongoClient } from 'mongodb';
+import config from './config/config';
+import mongo from './config/db.config';
 import { app } from './api/server';
 
-const port = process.env.PORT || 5000;
+const port = config.port;
 
-app.listen(port, () => {
-  console.log(`\n=== SERVER LISTENING ON ${port} ===\n`);
-});
+MongoClient.connect(mongo.url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(client => {
+    app.listen(port, () => {
+      console.log(`\n=== SERVER LISTENING ON ${port} ===\n`);
+    });
+  })
+  .catch(err => {
+    console.error(err.stack);
+    process.exit(1);
+  });
